d737172ba7f55b7367abd446f9bbc30d40588d84
yrk0265 <yrk0265@gmail.com>
2024-04-07 09:20:14 +0530

    1.Modified for disabling the formfield

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -9,10 +9,25 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CreateCourseStep2Component implements OnInit {
   form = this.fb.group({
-    courseType:['premium',Validators.required]
+    courseType:['premium',Validators.required],
+    price:[null,[
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern("[0-9]+")
+    ]]
   });
   constructor(private fb:FormBuilder){
   }
   ngOnInit() {
+    this.form.valueChanges.subscribe(val=>{
+      const priceControl=this.form.controls["price"];
+      if(val.courseType=='free' && priceControl.enabled){
+          priceControl.disable({emitEvent:false});
+      }
+      else if(val.courseType=='premium' && priceControl.disabled){
+          priceControl.enable({emitEvent:false});
+      }
+    })
   }
 }
